b8993dd331a464357cec6a5dc970e960f8054785
ngardunia <nickrhyse@gmail.com>
2023-01-26 21:11:53 -0500

    created addManager function to index.js

--- index.js
+++ index.js
@@ -11,20 +11,31 @@ const inquirer = require('inquirer');
 const generateHTML = require('./src/page-template');
 
 // empty team members array
+const teamMembers = [];
 // empty id array
-
+const teamId = [];
 
 // function that wraps everything (like an init function)
-
+function init () {
   // function for creating a manager (call this at the bottom of your init function)
+  const addManager = () => {
     // inquirer prompt with the manager questions
+    inquirer
+      .prompt ([
+
+      ])
+  
+    
     // in your .then - set up a variable for manager that is equal to a new instance of your Manager class passing in the responses you recieved from the user's input
-      .then(answers) => {
-        const manager = new Manager(answers.name, answers.office)
+      .then(answers => {
+        const manager = new Manager(answers.name, answers.email, answers.officenum)
+        const ident = (answers.id)
+        // push that variable to your team members array, push the id to the id array
         teamMembers.push(manager);
-      }
-    // push that variable to your team members array, push the id to the id array
+        teamId.push(ident);
+      })
       // call your next function
+    };
 
   // next function should be for creating the team
     // this should ask the user what type of employee they would like to create
@@ -34,7 +45,7 @@ const generateHTML = require('./src/page-template');
 
     function buildTeam() {
         // fs.writeFileSync(yourPathToDistFolder, functionFromPageTemplate(teamMembers), 'utf-8');
-      }
+      }};
   
-  
+    
   // make sure call your init function
