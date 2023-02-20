10b75bb996d7f14e069a727157725ca1db6cccc6
ngardunia <nickrhyse@gmail.com>
2023-02-20 18:13:11 -0500

    added test code for Engineer.test.js

--- ___tests___/Engineer.test.js
+++ ___tests___/Engineer.test.js
@@ -1,5 +1,18 @@
 // require Engineer
-
-//test the see if you can get a github account via constructor
+const Engineer = require("./Engineer");
+test("Can set GitHub account via constructor"), () => {
+    const engineer = new Engineer('Nick', 16231, 'mail@mail.com', 'ngardunia');
+    expect(engineer.github).toEqual(expect.any(String));
+}
 
 // test your get methods
+
+test("Can get GitHub username via getGitHub()"), () => {
+    const engineer = new Engineer('Nick', 16231, 'mail@mail.com', 'ngardunia');
+    expect(engineer.getGitHub()).toEqual(expect.any(String));
+}
+
+test("Can get role via getRole()"), () => {
+    const engineer = new Engineer('Nick', 16231, 'mail@mail.com', 'ngardunia');
+    expect(engineer.getRole()).toEqual(expect.any(String));
+}
